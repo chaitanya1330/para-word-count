d810a735e3997ae8c38087805005abb0c86ca84d
Gupta049 <sushilgupta049@gmail.com>
2026-02-15 23:03:29 +0530

    astdvbk

--- para_word_count/user/models.py
+++ para_word_count/user/models.py
@@ -16,7 +16,7 @@ class Paragraph(models.Model):
   user = models.ForeignKey(AuthUser, on_delete=models.CASCADE, related_name='paragraphs')
   raw_text = models.TextField()
   created_at = models.DateTimeField(auto_now_add=True)
-  
+   
   class Meta:
     ordering = ['-created_at']
 
